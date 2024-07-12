2d56ad33ed17d69f2cad5c8fab0f65a5320a0361
Francisco 'Franky' Alamo Garcia <fran.minecraft.mta@gmail.com>
2024-07-12 23:50:57 +0200

    fix: exit on empty or finish

--- src/discord-bot.ts
+++ src/discord-bot.ts
@@ -120,11 +120,14 @@ export const startDistubeBot = async (client: Client) => {
       else logger.error(e);
     })
     .on(Events.DELETE_QUEUE, (queue) => {
-      if (queue.textChannel) queue.textChannel.send("❌ | Queue deleted");
+      if (queue.voiceChannel) queue.voice.leave();
     })
     .on(
       Events.FINISH,
-      (queue) => queue.textChannel && queue.textChannel.send("Finished!"),
+      async (queue) =>
+        queue.textChannel &&
+        (await queue.textChannel.send("Finished!")) &&
+        queue.voice.leave(),
     );
 
   return distube;
